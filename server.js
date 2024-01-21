9396f5d847f749be9296668752214df7b424301f
Mandell1981 <Mandell1981.rm@gmail.com>
2024-01-21 13:06:12 -0500

    Complete-part-1

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 // CONFIRURATION
 require('dotenv').config()
 const PORT = process.env.PORT
+console.log(PORT)
 const app = express()
 
 // Routes
@@ -11,7 +12,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 // BREADS
-const breadController = require('./controllers/breads_controller.js')
+const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
 // LISTEN
