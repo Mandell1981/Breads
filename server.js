fb8d94f6277a875a3670a184b0f01dad75ba4926
Mandell1981 <Mandell1981.rm@gmail.com>
2024-01-24 23:54:28 -0500

    create and new routes complete

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
-// MIDDLEWARE
-app.use(express.static('public'))
+// Set Application
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
+
+// MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+// app.set('views', __dirname + '/views')
+// app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
 // Routes
